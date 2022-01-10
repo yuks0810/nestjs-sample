b5d95ed543906f6256427e74de3ecb3bbc69eef0
PC732_nakata_yuki <>
2022-01-10 19:20:18 +0900

    * gitignore編集 * typeormでDBに接続

--- nest-project-day1/src/app.module.ts
+++ nest-project-day1/src/app.module.ts
@@ -1,21 +1,26 @@
 import { Module } from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/typeorm-config.service';
 import { TasksModule } from './tasks/tasks.module';
 
 
 @Module({
   imports: [
+    /** env読み込み
+    *   環境変数NODE_ENVの値によって読み込むファイルを切り替える。
+    *   default.envは後続で呼ばれる。同じ変数がある場合は先に定義されているものが優先される。
+    */
+    ConfigModule.forRoot({
+      envFilePath: [`.env/${process.env.NODE_ENV}.env`, '.env/default.env'],
+      isGlobal: true,
+    }),
+    // TypeORMの設定を非同期取得に変更
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      port: 3306,
-      database: 'test_database',
-      host: 'db',
-      username: 'root',
-      password: 'root',
-      synchronize: true,
-      entities: [__dirname + '/**/*.entity.{js,ts}']
-    })
   ],
 })
 export class AppModule { }
